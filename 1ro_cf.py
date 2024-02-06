43e6f5bbe58a8c2848eb5cafa864104c24c1f007
amedina16 <104160873+amedina16@users.noreply.github.com>
2024-02-06 01:00:16 -0600

    dia1

--- 1ro_cf.py
+++ 1ro_cf.py
@@ -1,1 +1,8 @@
+nombre = input ('¿cual es tu nombre? ')
+apellidos= input ('¿cuales son tus apellidos? ')
+num_tel= int(input ('¿tienes numero telefonico? '))
+email= input ('¿cual es tu correo electronico? ')
+
+fullname= nombre+' '+apellidos
+print('Hola '+ fullname + '  en breve recibirás un correo a '+ email)
 
